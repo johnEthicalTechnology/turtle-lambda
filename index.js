70374862416f8e800cb531204088718ff51dcd44
johnEthicalTechnology <64393883+johnEthicalTechnology@users.noreply.github.com>
2020-04-27 14:49:25 +1000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function sleep(millis) {
-  return new Promise(resolve => setTimeout(resolve, millis));
-}
-
-module.exports = async (req, res) => {
-  await sleep(1000);
-  res.send('Waited 1 second');
-}
